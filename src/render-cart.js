c6e18bf21d9957b39d5a32ef97db632c55adb898
Quentin Leboucher <qleboucher1@gmail.com>
2024-02-12 23:07:54 +0100

    Modifs panier

--- src/render-cart.js
+++ src/render-cart.js
@@ -1,24 +1,24 @@
 const renderCartItems = (cart) => {
-    const cartContainer = document.getElementById('cart-container');
+  const cartContainer = document.getElementById("cart-container");
 
-    // Vérifie si le conteneur existe
-    if (!cartContainer) {
-        console.error("Le conteneur du panier n'a pas été trouvé dans le HTML.");
-        return;
-    }
+  // Vérifie si le conteneur existe
+  if (!cartContainer) {
+    console.error("Le conteneur du panier n'a pas été trouvé dans le HTML.");
+    return;
+  }
 
-    // Efface le contenu précédent du conteneur
-    cartContainer.innerHTML = '';
+  // Efface le contenu précédent du conteneur
+  cartContainer.innerHTML = "";
 
-    // Parcours chaque article dans le panier
-    cart.forEach(item => {
-        // Crée un élément div pour chaque article
-        const itemElement = document.createElement('div');
+  // Parcours chaque article dans le panier
+  cart.forEach((item) => {
+    // Crée un élément div pour chaque article
+    const itemElement = document.createElement("div");
 
-        // Ajoute le contenu HTML avec les détails de l'article
-        itemElement.innerHTML = `<p>ID: ${item.id}</p><p>Nom: ${item.name}</p><p>Prix: ${item.price}</p><p>Quantité: ${item.quantity}</p>`;
+    // Ajoute le contenu HTML avec les détails de l'article
+    itemElement.innerHTML = `<img src="../image/bijoux 2.jpg" height="80px"><p>Nom: ${item.name}</p><p>Prix: ${item.price}</p><p>Quantité: ${item.quantity}</p>`;
 
-        // Ajoute l'élément à la fin du conteneur
-        cartContainer.appendChild(itemElement);
-    });
-}
+    // Ajoute l'élément à la fin du conteneur
+    cartContainer.appendChild(itemElement);
+  });
+};
